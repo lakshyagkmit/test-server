cc161cebba0ba87c09b26d9ffec70561e7a344d8
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-10-09 17:21:55 +0530

    bugfix: solve error in initial get route

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =. {
-	res.send("message": "Hii from lakshya");
-})
+app.get('/', (req, res) => {
+	res.send({"message": "Hii from lakshya"});
+});
 
 app.get('/health', (req, res) => {
 	res.send({"message": "health ok!"});
